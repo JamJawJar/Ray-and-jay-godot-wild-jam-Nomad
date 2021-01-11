2da8f08814e9a295d29ee1d3a0de015f30048424
JamJawJar <66331482+JamJawJar@users.noreply.github.com>
2021-01-11 15:19:48 +0000

    Main scene can now load between scenes and menu now works

---
+++ Ray and James Game Jam Nomad/Scenes/Menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var scene_number_to_load = 1
+var load_scene_now = false
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Button_pressed() -> void:
+	load_scene_now = true

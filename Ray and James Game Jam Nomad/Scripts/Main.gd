e05359e27cf21546f0cc3e5be2259db210189d9c
JamJawJar <66331482+JamJawJar@users.noreply.github.com>
2021-01-13 18:22:52 +0000

    Finished forest level

--- Ray and James Game Jam Nomad/Scripts/Main.gd
+++ Ray and James Game Jam Nomad/Scripts/Main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var scenes = [preload("res://Scenes/Menu.tscn"), preload("res://Scenes/Level_Template.tscn")]
+var scenes = [preload("res://Scenes/Menu.tscn"), preload("res://Scenes/Forest_Level.tscn")]
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
